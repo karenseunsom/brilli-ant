011b4df48fc85002f34aebf54c9c9fccbc157573
Karen Seunsom <karenseuns@gmail.com>
2022-02-07 01:27:58 -0600

    add results.html

---
+++ db.js
@@ -0,0 +1,20 @@
+var categories = {
+    all: [
+        {
+            question: 'What is web development?',
+            answer: 'creating, building, and maintaining websites and web applications'
+        }
+    ],
+    css: [
+        {
+            question: 'What is CSS?',
+            answer: 'style sheet language used for describing the presentation of a document written in a markup language such as HTML'
+        }
+    ],
+    html:[
+        {
+            question: 'What is a div?',
+            answer: 'defines a division or a section in an HTML document'
+        }
+    ],
+}
---
+++ script.js
@@ -0,0 +1,6 @@
+const all = document.getElementById('option-all')
+
+all.addEventListener('click', () => {
+    console.log('hello')
+    console.log(categories.all)
+})
