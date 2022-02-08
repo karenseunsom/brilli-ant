32fdbd3f00e4b418b53bec43994ed0bc0106709f
Karen Seunsom <karenseuns@gmail.com>
2022-02-07 23:52:49 -0600

    add event listener to index cards to save category name in local storage and added more to ant logo

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-var categories = {
+const categories = {
     all: [
         {
             question: 'What is web development?',
--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const all = document.getElementById('option-all')
-
-all.addEventListener('click', () => {
-    console.log('hello')
-    console.log(categories.all)
+document.addEventListener('click', (event) => {
+    if (event.target.className == 'index-card') {
+        let clickedCategoryJSON = localStorage.getItem('clickedCategory');
+        let clickedCategory = JSON.parse(clickedCategoryJSON);
+        if (clickedCategory == null) {
+            clickedCategory = []
+        }
+        clickedCategory.splice(0, 1, event.target.dataset.name)
+        clickedCategoryJSON = JSON.stringify(clickedCategory)
+        localStorage.setItem('clickedCategory', clickedCategoryJSON)
+    }
 })
